2d0f7d6cd7aae210a3daa2ab8e3dd9ed7808e9a6
emilcyber <emil_babayev@hotmail.com>
2017-03-14 16:33:04 -0400

    second push

--- src/main/java/WebDriver_Demo_Test/WebDriver_Demo_Test/test_Git.java
+++ src/main/java/WebDriver_Demo_Test/WebDriver_Demo_Test/test_Git.java
@@ -3,5 +3,6 @@ package WebDriver_Demo_Test.WebDriver_Demo_Test;
 public class test_Git {
 public static void main(String[] args) {
 	System.out.println("first git transfer ");
+	System.out.println("second git transfer");
 }
 }
